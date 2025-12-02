756e6c9ca108fc526440c99c5ed3a691bf1a0043
m-senyshen <mattsenyshen@gmail.com>
2025-12-01 20:12:55 -0500

    Add pdf.worker.js with 404 Not Found content

---
+++ libs/pdf.worker.js
@@ -0,0 +1,15 @@
+
+<!-- saved from url=(0070)https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.2.67/pdf.worker.min.js -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"><title>404 Not Found</title></head>
+<body>
+<center><h1>404 Not Found</h1></center>
+<hr><center>nginx</center>
+
+
+
+
+
+
+
+
+</body></html>
